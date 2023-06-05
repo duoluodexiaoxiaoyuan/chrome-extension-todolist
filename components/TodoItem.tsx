db43d51b08c2fb309b7e51aab28e09e73ccc30eb
Developer27149 <564151947@qq.com>
2023-06-06 00:51:29 +0800

    feat: change status

--- components/TodoItem.tsx
+++ components/TodoItem.tsx
@@ -6,7 +6,8 @@ import { BsCalendar2Check } from "react-icons/bs"
 import { CiCircleCheck } from "react-icons/ci"
 
 import { calcTodoExprTime, formatTimestamp, isExprTimeExpired } from "~utils"
-import { editModelAtom } from "~utils/store"
+import { onModifyTodoItem } from "~utils/services"
+import { editModelAtom, todoListAtom } from "~utils/store"
 import { ETaskStatus, type ITodoItem } from "~utils/types"
 
 import CloseButton from "./CloseButton"
@@ -19,11 +20,28 @@ export default function TodoItem({ item, styles }: IProps) {
   const { taskName, taskContent, taskId, status, expectTime } = item
   const [isLoading, setIsLoading] = useState(false)
   const [editModal, setEditModal] = useAtom(editModelAtom)
+  const [_, setTodoList] = useAtom(todoListAtom)
   const onChangeStatus = async () => {
+    console.log("click change status")
     try {
       setIsLoading(true)
     } catch (error) {
       console.error("change status fail:", error)
+      const newTodoItem = await onModifyTodoItem({
+        ...item,
+        status:
+          item.status === ETaskStatus.已完成
+            ? ETaskStatus.未完成
+            : ETaskStatus.已完成
+      })
+      setTodoList((prev) => {
+        return prev.map((item) => {
+          if (item.taskId === taskId) {
+            return newTodoItem
+          }
+          return item
+        })
+      })
     } finally {
       setIsLoading(false)
     }
@@ -33,6 +51,7 @@ export default function TodoItem({ item, styles }: IProps) {
       className="p-4 pr-2 m-4 mx-8 border border-gray-100 rounded-md grid group grid-cols-todo border-l-[4px] hover:border-l-[#cb5647] hover:custom-shadow item-init"
       style={styles}>
       <button
+        onClick={onChangeStatus}
         className={clsx(
           "text-2xl hover:scale-105 origin-center transition-all",
           { "text-green-600": status === ETaskStatus.已完成 }
@@ -70,7 +89,7 @@ export default function TodoItem({ item, styles }: IProps) {
           )}>
           {calcTodoExprTime(expectTime)}
         </span>
-    </div>
+      </div>
       <div className="flex items-center justify-end gap-2 group-hover:opacity-100 opacity-0 ">
         <AiOutlineEdit
           onClick={() => {
